c1493cc7c58257333c403ccdec7d11eb8085c398
Juan Ponce <me.juanponce@gmail.com>
2026-09-16 22:45:15 +0000

    fix: add PrismaModule import to ComprasModule

--- apps/api/src/modules/compras/compras.module.ts
+++ apps/api/src/modules/compras/compras.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ComprasService } from './compras.service';
 import { ComprasController } from './compras.controller';
+import { PrismaModule } from '../../database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ComprasController],
   providers: [ComprasService],
   exports: [ComprasService],
