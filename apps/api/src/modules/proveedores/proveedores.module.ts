6cd058cfbd177813a19503c30df9e9aebc4920ce
Juan Ponce <me.juanponce@gmail.com>
2026-09-16 22:28:23 +0000

    fix: web Dockerfile copies .next and node_modules to production dir; proveedores module imports PrismaModule

--- apps/api/src/modules/proveedores/proveedores.module.ts
+++ apps/api/src/modules/proveedores/proveedores.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProveedoresService } from './proveedores.service';
 import { ProveedoresController } from './proveedores.controller';
+import { PrismaModule } from '../../database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProveedoresController],
   providers: [ProveedoresService],
   exports: [ProveedoresService],
